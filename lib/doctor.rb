de05eaff04fe4ab6c610528b3c5b0d25f49dd835
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 20:31:46 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,6 +11,13 @@ attr_accessor :name
     @name = name
     @@all << self
   end
-    
+  
+  def new_appointment(date, patient)
+    appointment = Appointment.new(date, patient)
+  end
+
+  def appointments
+    Appointment.all.select{|appointment| appointment.doctor == self}
+  end
 
 end
