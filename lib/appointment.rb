e9c52fbba4954b922a2ee54e93db96191f1b3278
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 20:37:54 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,7 +1,11 @@
 class Appointment
   attr_accessor :date, :patient, :doctor
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(date, patient, doctor)
 
-@@all = []
-  
-  
 end
