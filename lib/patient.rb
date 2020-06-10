9f67a88e9235b072a0f5b3de821ae90c5221c2aa
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-10 20:34:07 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,6 +6,9 @@ class Patient
     @@all
   end
 
-  def initialize
-
+  def new_appointment(date, doctor)
+    appointment = Appointment.new(date, doctor)
+    appointment.patient = self
+  end
+  
 end
